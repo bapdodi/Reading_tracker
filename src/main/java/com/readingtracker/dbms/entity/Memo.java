6046a9f5cf57f219b28b71adbd2e5963a34ce29f
Kim Jeong Bin <101189767+bapdodi@users.noreply.github.com>
2025-12-08 03:39:32 +0900

    Update Memo.java
    fix

--- src/main/java/com/readingtracker/dbms/entity/Memo.java
+++ src/main/java/com/readingtracker/dbms/entity/Memo.java
@@ -1,7 +1,8 @@
 package com.readingtracker.dbms.entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -10,9 +11,18 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import com.readingtracker.dbms.repository.MemoRepository;
 import com.sharedsync.shared.annotation.CacheEntity;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 @CacheEntity
 @Entity
 @Table(name = "memo")
@@ -108,6 +118,10 @@ public class Memo {
     public Long getUserId() { return user != null ? user.getId() : null; }
     public Long getUserShelfBookId() { 
         return userShelfBook != null ? userShelfBook.getId() : null; 
+    }
+
+    public Long getId() {
+        return id;
     }
 }
 
